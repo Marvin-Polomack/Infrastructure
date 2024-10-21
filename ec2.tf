368365a25c123009187936aa7c35ee42db6f42f1
mpolomack <marvin.polomack@gmail.com>
2024-10-21 15:55:36 +0200

    correct ami in aws ec2_k8s

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_k8s" {
-  ami           = data.aws_ami.amazon_linux_2023
+  ami           = data.aws_ami.amazon_linux_2023.image_id
   instance_type = "t2.micro"
 
   tags = {
