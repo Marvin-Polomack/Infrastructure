17929cb809fa6a2eafe559475beb141f3a11f1df
mpolomack <marvin.polomack@gmail.com>
2024-12-20 19:49:28 +0100

    Create theworldismind.com github actions IAM role

--- iam.tf
+++ iam.tf
@@ -11,7 +11,7 @@ module "theworldismind_github_action_iam_role" {
 
     github_org        = "Marvin-Polomack"
     repository_name   = "theworldismind.com"
-    oidc_provider_arn = module.infrastructure_github_action_iam_role.role_arn
+    oidc_provider_arn = module.infrastructure_github_action_iam_role.oidc_provider_arn 
 
     depends_on = [ module.infrastructure_github_action_iam_role ]
 }
