03cdcf61a16312e35c4ca6483ed0c423c821721a
mpolomack <marvin.polomack@gmail.com>
2024-12-20 19:15:59 +0100

    Create theworldismind.com github actions IAM role

--- iam.tf
+++ iam.tf
@@ -1,6 +1,13 @@
-module "github_action_iam_role" {
+module "infrastructure_github_action_iam_role" {
     source = "./modules/iam-role"
 
     github_org      = "Marvin-Polomack"
     repository_name = "Infrastructure"
+}
+
+module "theworldismind_github_action_iam_role" {
+    source = "./modules/iam-role"
+
+    github_org      = "Marvin-Polomack"
+    repository_name = "theworldismind.com"
 }
